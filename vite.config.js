f2feaca722906ff75537cb46893615ab9d42b941
vitalya.efimov <vitalay.efimov@gmail.com>
2024-12-06 15:53:09 +0500

    1

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/19-prilogenie-gmail/',
+  base: '/19-prilogenie-gmail',
   plugins: [
    
     vue(),
